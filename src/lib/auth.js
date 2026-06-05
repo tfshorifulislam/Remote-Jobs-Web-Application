b25ddd060f9551198bd9a1ffb2677274da472f56
tfshorifulislam <tfshorifulislam@gmail.com>
2026-06-05 20:39:46 +0600

    sing up system done

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,8 +2,8 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017/database");
-const db = client.db();
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db(process.env.MONGODB_NAME || "better-auth");
 
 export const auth = betterAuth({
     emailAndPassword: {
