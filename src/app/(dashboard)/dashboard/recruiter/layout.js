e40b845868c301b7fb0851d2472020c9f9a31cf5
tfshorifulislam <tfshorifulislam@gmail.com>
2026-06-07 00:10:55 +0600

    recruiter dashboard layout bg color update

--- src/app/(dashboard)/dashboard/recruiter/layout.js
+++ src/app/(dashboard)/dashboard/recruiter/layout.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const DashboardLayout = ({ children }) => {
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen bg-[#131314]">
 
             {/* Sidebar */}
             <div className="
